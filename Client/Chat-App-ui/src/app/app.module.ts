122362ceb79b27a1e1c22b802f365eb4672084b8
abdulhanan897 <ah350868@gmail.com>
2021-04-02 21:25:25 +0500

    add component

--- Client/Chat-App-ui/src/app/app.module.ts
+++ Client/Chat-App-ui/src/app/app.module.ts
@@ -3,28 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ChatRoomComponent } from './chat-room/chat-room.component';
+import { ChatRoomComponent } from './pages/chat-room/chat-room.component';
 import { FormsModule } from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
-import {MatIconModule} from '@angular/material/icon';
 import { HttpClientModule } from '@angular/common/http';
-import {MatButtonModule} from '@angular/material/button';
-
+import { LoginComponent } from './pages/login/login.component';
+import { AngularMaterialModule } from './angularMaterial.module';
+import { AppRoutingModule } from './app-routing.module';
+import { SignupComponent } from './pages/signup/signup.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChatRoomComponent
+    ChatRoomComponent,
+    LoginComponent,
+    SignupComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
-    MatInputModule,
-    MatIconModule,
     HttpClientModule,
-    MatButtonModule
+    AngularMaterialModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
