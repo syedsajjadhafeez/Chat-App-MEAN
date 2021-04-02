0fbe1132c7c03c480abf8f3f363788d1ebb95ea0
syedsajjadhafeez <syedsajjadhafeez@gmail.com>
2021-04-02 17:09:01 +0500

    mark as read route change to get

--- server/routes/chatRoom.js
+++ server/routes/chatRoom.js
@@ -9,6 +9,6 @@ router
   .get('/:roomId', chatRoom.getConversationByRoomId)
   .post('/initiate', chatRoom.initiate)
   .post('/:roomId/message', chatRoom.postMessage)
-  .put('/:roomId/mark-read', chatRoom.markConversationReadByRoomId)
+  .get('/:roomId/mark-read', chatRoom.markConversationReadByRoomId)
 
 export default router;
