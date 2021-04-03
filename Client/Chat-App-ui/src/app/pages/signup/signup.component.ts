b944ffa41307fb4c4c6ce9ff9374f6f7a36d400c
Najam Agha <na1amagha@gmail.com>
2021-04-03 19:41:58 +0500

    (feat) : add user signup

--- Client/Chat-App-ui/src/app/pages/signup/signup.component.ts
+++ Client/Chat-App-ui/src/app/pages/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  firstName: string;
+  lastName: string;
+  username: string;
+  type = 'consumer';
 
+  constructor(private userService: UserService, private router: Router) { }
   ngOnInit(): void {
   }
 
+  async register() {
+    let usernameRegex = /^[a-zA-Z0-9]+$/;
+    let isValidUsername = this.username.match(usernameRegex);
+    try {
+      if (isValidUsername
+        && (this.firstName && this.firstName.length > 0)
+        && (this.lastName && this.lastName.length > 0)
+        && (this.username && this.username.length > 0)) {
+        let data: User = {
+          "firstName": this.firstName,
+          "lastName": this.lastName,
+          "type": "consumer",
+          "username": this.username
+        };
+        let response = await this.userService.createUser(data).toPromise();
+
+        if (response.success) {
+          alert('Registration Successful')
+          this.router.navigate(['login']);
+        }
+      }else{
+        alert('Invalid username or form data missing')
+      }
+    } catch (error) {
+
+    }
+
+  }
 }
